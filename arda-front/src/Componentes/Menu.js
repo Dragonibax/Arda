8a90b438b2762b9ecd3424bfeba13ff782610e6d
David Perez Antonio <david.perez.antonio51@gmail.com>
2022-01-19 13:55:43 -0600

    Se crea menu de navegacion

--- arda-front/src/Componentes/Menu.js
+++ arda-front/src/Componentes/Menu.js
@@ -1,31 +1,41 @@
 import React from 'react'
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import {GiTripleClaws} from 'react-icons/gi'
 
 function Menu(props) {
     return (
-        <>
-            <Container fluid>
-                <Navbar bg="light" expand="lg">
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                     <Container fluid>
-                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
+                        <Navbar.Brand href="#home">
+                            <GiTripleClaws className="d-inline-block align-top"/>
+                            {' '}
+                            ARDA
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
                             <Nav className="me-auto">
-                                <Nav.Link href="#home">Home</Nav.Link>
-                                <Nav.Link href="#link">Link</Nav.Link>
-                                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                <Nav.Link href="#features">Monitor</Nav.Link>
+                                <Nav.Link href="#pricing">Aquirir un terrario</Nav.Link>{/* Condicionar segun inicio de Sesion*/ }
+                                <NavDropdown title="Administar Iguanas" id="collasible-nav-dropdown">
+                                    <NavDropdown.Item href="#action/3.1">Registar Iguana</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Ver iguanas</NavDropdown.Item>
                                 </NavDropdown>
+                                <NavDropdown title="Administar Terrarios" id="collasible-nav-dropdown">
+                                    <NavDropdown.Item href="#action/3.1">Monitorear Terrario</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">Editar datos de terrario</NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#action/3.4">Registrar un Terrario</NavDropdown.Item>
+                                </NavDropdown>
+                            </Nav>
+                            <Nav>
+                                <Nav.Link href="#deets">Perfil</Nav.Link>
+                                <Nav.Link eventKey={2} href="#memes">
+                                    Cerrar Sesion
+                                </Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
-            </Container>
-        </>
     );
 }
 
