bdc1595c91fa93578dbbe647b264a3a83bfd8b79
Dragonibax <sabino.snm@gmail.com>
2022-11-15 19:11:42 -0600

    Alertas back terminadas

--- Arda/src/main/java/com/escom/ipn/Arda/DTO/MESSAGES.java
+++ Arda/src/main/java/com/escom/ipn/Arda/DTO/MESSAGES.java
@@ -1,7 +1,25 @@
 package com.escom.ipn.Arda.DTO;
 
 public class MESSAGES {
-
-    public static final String ALERTA_TEMPERATURA = "¡Alerta desde Arda!";
-    public static final String DESCRIPCION_TEMPERATURA_ALTA = "Estimado %s se le recomienda revisar de inmediato los ventiladores o focos para verificar la causa de la falla, en caso de necesitar ayuda por favor llame a servicio tecnico lo mas pronto posible";
+    
+    //Temperatura Alta
+    public static final String ALERTA_TEMPERATURA_A = "¡Alerta desde Arda! TEMPERATURA ELEVADA";
+    public static final String DESCRIPCION_TEMPERATURA_ALTA = "Estimado %s , le notificamos que la temperatura dentro del terrario es demaciado alta (Mas de 34° C). El terrarrio ya se encuentra tratando de regular este problema, aun asi se le recomienda revisar de inmediato los ventiladores o focos para verificar la causa de la falla, en caso de necesitar ayuda por favor llame a servicio tecnico lo mas pronto posible";
+    //Humedad Alta
+    public static final String ALERTA_HUMEDAD_A = "¡Alerta desde Arda! HUMEDAD ALTA";
+    public static final String DESCRIPCION_HUMEDAD_ALTA = "Estimado %s , le notificamos que la humedad dentro del terrario es demaciado alta (Mas de 80%). El terrarrio ya se encuentra tratando de regular este problema, aun asi se le recomienda revisar de inmediato los ventiladores o el humidificador para verificar la causa de la falla, en caso de necesitar ayuda por favor llame a servicio tecnico lo mas pronto posible";
+  
+    public static final String ALERTA_TEMPERATURA_B = "¡Alerta desde Arda! TEMPERATURA BAJA";
+    public static final String DESCRIPCION_TEMPERATURA_BAJA = "Estimado %s , le notificamos que la temperatura dentro del terrario es demaciado baja (Menos del 20° C). El terrarrio ya se encuentra tratando de regular este problema, aun asi se le recomienda revisar de inmediato los ventiladores o focos para verificar la causa de la falla, en caso de necesitar ayuda por favor llame a servicio tecnico lo mas pronto posible";
+  
+    public static final String ALERTA_HUMEDAD_B = "¡Alerta desde Arda! HUMEDAD BAJA";
+    public static final String DESCRIPCION_HUMEDAD_BAJA = "Estimado %s , le notificamos que la humedad dentro del terrario es demaciado baja (Menos del 40%). El terrarrio ya se encuentra tratando de regular este problema, aun asi se le recomienda revisar de inmediato los ventiladores o el humidificador para verificar la causa de la falla, en caso de necesitar ayuda por favor llame a servicio tecnico lo mas pronto posible";
+  
+    public static final String ALERTA_TEMPERATURA_C = "¡Alerta desde Arda! Problema en los sensores";
+    public static final String DESCRIPCION_TEMPERATURA_SENSOR = "Estimado %s , le notificamos que por algun motivo hay una gran diferencia de temperatura en el terrarrio sugerimos que revise el funcionamiento de los sensores lo mas pronto posible para que el sistema pueda funcionar de manera adecuada, en caso de necesitar ayuda por favor llame a servicio tecnico lo mas pronto posible";
+  
+    public static final String ALERTA_HUMEDAD_C = "¡Alerta desde Arda! Problema en los sensores";
+    public static final String DESCRIPCION_HUMEDAD_SENSOR = "Estimado %s , le notificamos que por algun motivo hay una gran diferencia de humedad en el terrarrio sugerimos que revise el funcionamiento de los sensores lo mas pronto posible para que el sistema pueda funcionar de manera adecuada, en caso de necesitar ayuda por favor llame a servicio tecnico lo mas pronto posible";
+ 
+    
 }
--- Arda/src/main/java/com/escom/ipn/Arda/Servicios/EmailsServicio.java
+++ Arda/src/main/java/com/escom/ipn/Arda/Servicios/EmailsServicio.java
@@ -34,17 +34,67 @@ public class EmailsServicio {
         SimpleMailMessage correo = new SimpleMailMessage();
         String message = "";
         switch (alertType) {
+        // 0 para todo bien
+        // 1 para temperatura muy alta
+        // 2 Para humedad muy alta
+        // 3 Temperatura muy baja
+        // 4 Humedad muy baja
+        // 5 la medicion de temperatura vario mucho
+        // 6 la medicion de humedad vario mucho
+            
             case 1:
                 message = MESSAGES.DESCRIPCION_TEMPERATURA_ALTA;
                 message = String.format(message,nombre);
                 correo.setFrom(from);
                 correo.setTo(user.get().getCorreo());
-                correo.setSubject(MESSAGES.ALERTA_TEMPERATURA);
-                
+                correo.setSubject(MESSAGES.ALERTA_TEMPERATURA_A);                
                 correo.setText(message);
                 break;
+                
             case 2:
+                message = MESSAGES.DESCRIPCION_HUMEDAD_ALTA;
+                message = String.format(message,nombre);
+                correo.setFrom(from);
+                correo.setTo(user.get().getCorreo());
+                correo.setSubject(MESSAGES.ALERTA_HUMEDAD_A);                
+                correo.setText(message);
                 break;
+                
+            case 3:
+                message = MESSAGES.DESCRIPCION_TEMPERATURA_BAJA;
+                message = String.format(message,nombre);
+                correo.setFrom(from);
+                correo.setTo(user.get().getCorreo());
+                correo.setSubject(MESSAGES.ALERTA_TEMPERATURA_B);                
+                correo.setText(message);
+                break;
+                
+            case 4:
+                message = MESSAGES.DESCRIPCION_HUMEDAD_BAJA;
+                message = String.format(message,nombre);
+                correo.setFrom(from);
+                correo.setTo(user.get().getCorreo());
+                correo.setSubject(MESSAGES.ALERTA_HUMEDAD_B);                
+                correo.setText(message);
+                break;
+            
+            case 5:
+                message = MESSAGES.DESCRIPCION_TEMPERATURA_SENSOR;
+                message = String.format(message,nombre);
+                correo.setFrom(from);
+                correo.setTo(user.get().getCorreo());
+                correo.setSubject(MESSAGES.ALERTA_TEMPERATURA_C);                
+                correo.setText(message);
+                break;
+                
+            case 6:
+                message = MESSAGES.DESCRIPCION_HUMEDAD_SENSOR;
+                message = String.format(message,nombre);
+                correo.setFrom(from);
+                correo.setTo(user.get().getCorreo());
+                correo.setSubject(MESSAGES.ALERTA_HUMEDAD_C);                
+                correo.setText(message);
+                break;    
         }
         mailSender.send(correo);
     }
